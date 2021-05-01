4430993452356216601759560b39a0d7ebebfa2e
Arsh Ahluwalia <arsh.ahluwalia@hotmail.com>
2021-05-01 00:01:13 -0400

    Update 1_two_sum.cpp

--- easy/1_two_sum.cpp
+++ easy/1_two_sum.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         std::vector<int> answer;
+        
         for (int i = 0; i < nums.size(); i++) {               // brute force (2 for loops)
             for (int j = (i + 1); j < nums.size(); j++) {
                 if (nums[i] + nums[j] == target) {
