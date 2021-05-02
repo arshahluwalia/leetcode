1fb1e06456b01f8b8d40e69bc25eebb8fa569377
Arsh Ahluwalia <arsh.ahluwalia@hotmail.com>
2021-05-02 01:04:22 -0400

    Update 7_reverse_integer.cpp

--- easy/7_reverse_integer.cpp
+++ easy/7_reverse_integer.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int reverse(int x) {
-        int reverse = 0;
+        int reverse = 0;                                                                        // reverse is zero before iteration
         
         while (x != 0) {
-            int digit = x % 10;
+            int digit = x % 10;                                                                 // popping digit
             x /= 10;
             
-            if ((reverse > INT_MAX/10) || ((reverse == INT_MAX/10) && (digit > 7))) {
+            if ((reverse > INT_MAX/10) || ((reverse == INT_MAX/10) && (digit > 7))) {           // checking for positive overflow
                 return 0;
             }
             
-            if ((reverse < INT_MIN/10) || ((reverse == INT_MIN/10) && (digit < -8))) {
+            if ((reverse < INT_MIN/10) || ((reverse == INT_MIN/10) && (digit < -8))) {          // checking for negative overflow
                 return 0;
             }
             
-            reverse = (reverse * 10) + digit;
+            reverse = (reverse * 10) + digit;                                                   // pushing digit
         }
         
         return reverse;
